62113b37bc26cf5b540b4ddb97f74598204ca500
potlitel <potlitel@gmail.com>
2022-06-22 05:46:45 -0400

    Se adiciona funciona para compartir desde el propio dispositivo movil, falla desde el emulador en la web.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -9,30 +9,57 @@ import {
   TouchableOpacity,
 } from "react-native";
 import logo from "./assets/react-native.png";
-import * as ImagePicker from "expo-image-picker"
+import * as ImagePicker from "expo-image-picker";
+import * as Sharing from "expo-sharing";
+
 const App = () => {
+  const [selectedImage, setselectedImage] = useState(null);
 
   let openImagePickerAsync = async () => {
-    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
-    if(permissionResult.granted === false){
-      alert('Permission to access camera is required');
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera is required");
       return;
     }
 
-    const pickerResult = await ImagePicker.launchImageLibraryAsync()
-    console.info(pickerResult);
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    //console.info(pickerResult);
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setselectedImage = { localuri: pickerResult.uri };
+  };
+
+  let openShareDialog = async () => {
+    if((! await Sharing.isAvailableAsync())){
+      alert("Sharing is not available on your platform");
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localuri);
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World!!!</Text>
-      <Image source={logo} style={styles.image} />
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => Alert.alert("Hello!!!")}
-      >
-        <Text style={styles.buttonText}>Press Here</Text>
+      <Text style={styles.title}>Pick an Image!!!</Text>
+
+      <TouchableOpacity onPress={openImagePickerAsync}>
+        <Image
+          source={selectedImage !== null ? selectedImage.localuri : logo}
+          style={styles.image}
+        />
       </TouchableOpacity>
+
+      {
+        selectedImage ? (<TouchableOpacity
+        style={styles.button}
+        onPress={openShareDialog}
+      >
+        <Text style={styles.buttonText}>Share this image</Text>
+      </TouchableOpacity>) : <View/>
+      }
     </View>
   );
 };
@@ -45,9 +72,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   title: { fontSize: 30, color: "#fff" },
-  image: { height: 200, width: 200, borderRadius: 100 },
-  button: { backgroundColor: "deepskyblue", padding: 7, marginTop: 10 , borderRadius:10},
-  buttonText:{color:'#fff', fontSize:20}
+  image: { height: 200, width: 200, borderRadius: 100, resizeMode: "contain" },
+  button: {
+    backgroundColor: "deepskyblue",
+    padding: 7,
+    marginTop: 10,
+    borderRadius: 10,
+  },
+  buttonText: { color: "#fff", fontSize: 20 },
 });
 
 export default App;
